3fa0040ad28ae859d47f253f1dd6b9128329c874
Meonardo <sisxme@gmail.com>
2021-05-08 17:59:15 +0800

    Rebuild WebRTC library.

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/Meonardo/WebRTC/releases/download/90.1.4/WebRTC-M90.1.4.xcframework.zip",
-            checksum: "8d3730cfa29ebadb9ef13c2abf123240ee369e9ae6c3d6e7b189af8c85f38889"
+            url: "https://github.com/Meonardo/WebRTC/releases/download/90.1.5/WebRTC-M90.1.5.xcframework.zip",
+            checksum: "48c81249c62cafdfbbe34a91cab3490cfcd1d324eef7a514cf78adf3a785a8cd"
         ),
     ]
 )
